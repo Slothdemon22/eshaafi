5b230b0b3f21513edce2df382fd7f2b7551c76a8
Slothdemon22 <www.basilslothdemon@gmail.com>
2025-09-02 23:58:16 +0500

    CICD job

--- backend/src/index.js
+++ backend/src/index.js
@@ -45,4 +45,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT =  5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
